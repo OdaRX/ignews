d99db55b459ab7c255facd2dae55e487e66d9936
unknown <chromegold70@gmail.com>
2021-10-21 21:01:29 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+// import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, wellcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
