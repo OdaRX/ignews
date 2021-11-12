186e72ae6f922dbbe1eb6b9d30537610ecf2a1a7
unknown <chromegold70@gmail.com>
2021-11-12 10:54:18 -0300

    Fix session

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const { slug } = params
   const prismic = getPrismicClient(req)
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
